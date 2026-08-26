f5f2361c6b03d7368ff00d9f0b61913c124e2ced
DreamJain10 <dreampachori18@gmail.com>
2026-08-26 22:30:43 +0530

    Star Pattern

--- Swap.java
+++ Swap.java
@@ -1,12 +1,16 @@
 import java.util.Scanner;
 
-public class Main {
+public class Swap {
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
         System.out.print("ENTER THE VALUE OF A: ");
         int a = input.nextInt();
         System.out.print("enter the value of b: ");
         int b = input.nextInt();
+        /*
+        only if our no. is more than 0
+        if no. is 0 then we have to use temporary variable
+         */
         a = a*b;
         b = a/b;
         a = a/b;
